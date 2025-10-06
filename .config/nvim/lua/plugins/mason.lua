ed48c1751773209fa0eba4ab926da76d721693de
xueyp <xueyp.gtd@gmail.com>
2025-10-06 13:49:26 +0800

    mason-org

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -1,7 +1,7 @@
 return {
   {
 
-    "williamboman/mason.nvim",
+    "mason-org/mason.nvim",
     opts = {
       ensure_installed = {
         'bash-language-server', 'r-languageserver', 'clangd', 'gopls', 'pyright', 'jdtls', 'prettier',
